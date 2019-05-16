4772cb0453a46cf7da6393218d6fa5435e6b9f99
Nina Johnson <9makesthings@gmail.com>
2019-05-16 00:05:27 -0500

    Updates

---
+++ precache-manifest.14bee65486b3996df42bf3f2c1f4991d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6cd8ec5b4e89fb46a839e6d015d1b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f22c1f6a4c90f3b619a",
+    "url": "/static/css/main.c8f3cc91.chunk.css"
+  },
+  {
+    "revision": "63f0d158a4f07a4c15e7",
+    "url": "/static/js/2.c21ed767.chunk.js"
+  },
+  {
+    "revision": "5f22c1f6a4c90f3b619a",
+    "url": "/static/js/main.e699efe0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
